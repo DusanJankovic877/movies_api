88e46bf00138bc8e3e594450d8869b2b75c5a95e
Dusan <dusanjankovic877@gmail.com>
2020-10-31 13:57:59 +0100

    created CRUD from movies and tested it with postman

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Movie;
 class MovieController extends Controller
 {
     //
+    public function index(){
+        $movies = Movie::all();
+        return response()->json($movies);
+    }
+    public function show($id) {
+        $movie = Movie::findOrFail($id);
+        return response()->json($movie);
+
+    }
+    public function store(Request $request){
+        $movie = $request;
+        // dd($movie['title']);
+
+        Movie::create([
+            'title' => $movie['title'],
+            "director" => $movie['director'],
+            "image_url" => $movie['image_url'],
+            "duration" => $movie['duration'],
+            "release_date" => $movie['release_date'],
+            "genre" => $movie['genre'],
+
+        ]);
+        return $movie;
+    }
+    public function update(Request $request, $id){
+
+        $movie = Movie::findOrFail($id);
+
+        $movie->update([
+            'title' => $request['title'],
+            "director" => $request['director'],
+            "image_url" => $request['image_url'],
+            "duration" => $request['duration'],
+            "release_date" => $request['release_date'],
+            "genre" => $request['genre'],
+        ]);
+        return $movie;
+
+
+    }
+    public function destroy($id){
+        $movie = Movie::findOrFail($id);
+        $movie->delete();
+        return $movie;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies/{id}', [MovieController::class, 'show']);
+Route::post('/movies', [MovieController::class, 'store']);
+Route::put('/movies/{id}', [MovieController::class, 'update']);
+Route::delete('/movies/{id}', [MovieController::class, 'destroy']);
